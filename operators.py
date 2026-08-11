b82498a1c87e55f594dafea2721910544a766efa
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-11 15:59:32 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,11 @@
+a = 7
+b = 17
+print(a + b) # Addition.
+print(a - b) # Subtraction.
+print(a*b)   # Multiplication.
+print(a/b) # Division that gives quotient only in float.
+print(a % b) # Modulus that gives you remainder.
+print ( a // b) # Division that gives you quotient in integer format.
+print(a ** b ) # Exponential
+
+
