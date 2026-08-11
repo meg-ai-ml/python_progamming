61fe6037de4383f7f99966eeef208c28494923e2
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-11 16:01:47 +0530

    Enhance type checks with output comments
    Added comments to clarify the output of type checks.

--- data_types.py
+++ data_types.py
@@ -1,9 +1,12 @@
+# This program shows how to find out the type of data stored in a variable.
+
+
 name = "Meghana" # String
 roll_no = 7 # Integer
 percentage = 99.9 # Float
 present = True # Boolean
 
-print(type(name))
-print(type(roll_no))
-print(type(percentage))
-print(type(present))
+print(type(name)) # <class 'str'>
+print(type(roll_no)) # <class 'int'>
+print(type(percentage)) # <class 'float'>
+print(type(present)) # <class 'bool'>
