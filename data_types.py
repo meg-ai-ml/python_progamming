a69fee74d1263cd67db7fdb027c476c7e7484d82
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-11 15:59:03 +0530

    Add data types examples in data_types.py

---
+++ data_types.py
@@ -0,0 +1,9 @@
+name = "Meghana" # String
+roll_no = 7 # Integer
+percentage = 99.9 # Float
+present = True # Boolean
+
+print(type(name))
+print(type(roll_no))
+print(type(percentage))
+print(type(present))
